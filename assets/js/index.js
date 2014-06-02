17626d24b51da96f9a2f01bbd4f92bc2b1be1512
Lillian Chen <qc53@cornell.edu>
2014-06-02 17:05:59 -0400

    base homepage done
    working on psd mockups now

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+// $( document ).ready(function() {
+// 	function downLoad(){
+// 	  if (document.all){
+// 	      document.all["layer1"].style.visibility="hidden";
+// 	      document.all["layer2"].style.visibility="visible";
+// 	  } else if (document.getElementById){
+// 	      node = document.getElementById("layer1").style.visibility='hidden';
+// 	      node = document.getElementById("layer2").style.visibility='visible';
+// 	  }
+// 	}
+
+// 	$(window).load(function() {
+	
+// 	})
+// });
