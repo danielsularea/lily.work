352b86bd3e4e7316d76bd4b52e6998cb7f98c5e9
Lily <lilyc5459@gmail.com>
2014-08-01 16:17:15 -0400

    dynamic navbar on scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,3 +13,29 @@
 	
 // 	})
 // });
+
+$( document ).ready(function() {
+	$(window).scroll(function () {
+	  if ($(document).scrollTop() < 150) {
+	    $('.lc-nav').removeClass('lc-tiny-nav');
+	    $('.lc-nav-link-top').addClass('hidden');
+	  } else {
+	    $('.lc-nav').addClass('lc-tiny-nav');
+	    $('.lc-nav-link-top').removeClass('hidden');
+	  }
+	});
+
+  $('.lc-nav-link-top').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
